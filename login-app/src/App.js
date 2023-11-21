2fb4f3d9d9773962e556743f0f63ca598060dd34
CzarCerro <abramobligacion@gmail.com>
2023-11-21 19:55:18 +0800

    init: skeleton pages

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import LoginPage from './pages/LoginPage';
+import WelcomePage from './pages/WelcomePage';
+import ManagerPage from './pages/ManagerPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={<LoginPage
+            />}
+          />
+          <Route
+            path="/welcome"
+            element={<WelcomePage
+            />}
+          />
+          <Route
+            path="/manager"
+            element={<ManagerPage
+            />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
